4f880ea7d10c9d02695beccc76a95ffa90ac5684
not-the <87151784+not-the@users.noreply.github.com>
2022-11-02 19:16:19 -0400

    first commit

---
+++ file_system.js
@@ -0,0 +1,290 @@
+var file_system = {
+    'desktop': {
+        type: 'dir',
+
+        'Example.txt': {
+            data: 'Hello. This is a sample text file. Try editing it.',
+        },
+        'Another file.txt': {
+            data: 'another file!!!!!!!!11!1!'
+        },
+        'SE wallpaper.img': {
+            data: './assets/wallpaper/se1.png',
+        },
+        'sml.txt': {
+            data: 'small filename',
+        },
+        'really long file name.txt': {
+            data: 'well',
+        },
+    },
+    'documents': {
+        type: 'dir',
+        'A Document.txt': {
+            data: 'Important information',
+        },
+    },
+    'apps': {
+        type: 'dir',
+
+        'files': {
+            title: 'Files',
+            icon: './assets/icon/icons8-file-folder-48.png',
+            file_icon: './assets/icon/icons8-file-folder-48.png',
+            html: /*html*/ `
+                <style>
+                    .app_files .content {
+                        padding: 6px 8px;
+                        display: flex;
+                        flex-direction: column;
+                    }
+                    .app_files .content .status_bar {
+                        border-top: 1px solid var(--border-color);
+                        padding-top: 3px;
+                    }
+                    .app_files .content .inner {
+                        width: 100%;
+                        height: 100%;
+                        margin-bottom: 6px;
+
+                        display: flex;
+                    }
+                    .app_files .content .pane {
+                        width: 100%;
+                        min-width: 100px;
+                        max-width: 200px;
+                        resize: horizontal;
+                        margin-top: 12px;
+                        padding-right: 12px;
+                        border-right: 1px solid var(--border-color);
+
+                    }
+                    .app_files .content .pane button {
+                        color: var(--text-alt);
+                        font-size: 11pt;
+                        text-align: left;
+                        cursor: pointer;
+
+                        width: 100%;
+                        margin: 1px 6px;
+                        padding: 3px 6px;
+                        border-radius: 3px;
+                        border-bottom: 1px solid var(--border-color);
+                        transition: 0.05s background-color ease-in;
+                    }
+                    .app_files .content .pane button:hover {
+                        color: var(--text);
+                        border-color: transparent;
+                        background-color: var(--hover-color);
+                        transition: unset;
+                    }
+                    .app_files .content .pane button.active {
+                        color: var(--text);
+                        border-color: transparent;
+                        background-color: var(--border-color);
+                    }
+
+                </style>
+
+                <div class="options">
+                    BUTTONS GO HERE
+                </div>
+
+                <div class="inner">
+                    <div class="pane">
+                        <button class="active" data-directory-button="~/desktop">Desktop</button>
+                        <button data-directory-button="~/documents">Documents</button>
+                        <button data-directory-button="~/apps">Apps</button>
+                        <button data-directory-button="~/system">System</button>
+                    </div>
+                    <div class="view_folder state_icon" data-directory="~/desktop">
+
+                    </div>
+                </div>
+                <div class="status_bar">11 files</div>
+            `,
+            script: (stage='start', proc, path) => {
+                // Navigate function
+                proc.memory.navigate = dir => {
+                    let container = document.querySelector(`#app_container .window[data-process="${proc.id}"] .view_folder`);
+                    container.setAttribute("data-directory", dir);
+                    populateFolder(dir);
+                }
+
+                // Buttons
+                let buttons = document.querySelectorAll(`#app_container .window[data-process="${proc.id}"] button[data-directory-button]`);
+                buttons.forEach(element => {
+                    element.addEventListener('click', event => {
+                        try {
+                            proc.memory.navigate(event.srcElement.dataset.directoryButton);
+                            buttons.forEach(e => e.classList.remove('active'));
+                            element.classList.add('active');
+                        } catch (error) { console.warn('Invalid directory'); console.error(error);  }
+                    })
+                })
+
+                populateFolder(path);
+            },
+
+            window: {
+                width: '800',
+                height: '500',
+            },
+        },
+
+        'notes': {
+            title: 'Notes',
+            icon: './assets/icon/Bloc_Notes_SZ.png',
+            file_icon: './assets/icon/blank_document.png',
+            html: /*html*/ `
+                <style>
+                    .app_notes .content {
+                        padding: 6px 8px;
+                        display: flex;
+                        flex-direction: column;
+                    }
+                    .app_notes .content textarea {
+                        width: 100%;
+                        height: 100%;
+                        font-family: var(--font-system);
+                        font-size: 11pt;
+                        background: none;
+                        color: var(--text);
+                        border: none;
+                        outline: none;
+                        resize: none;
+                    }
+                    .app_notes .content .status_bar {
+                        border-top: 1px solid var(--border-color);
+                        padding-top: 3px;
+                    }
+                </style>
+
+                <textarea name="" id="" cols="30" rows="10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis quae praesentium rem qui sit laborum, iure dolor ex quibusdam illo quia aut similique, nisi voluptatibus. Ex fugit quae explicabo iusto, molestias minus? Omnis animi qui harum esse labore, quia quae aliquid, eligendi totam autem, blanditiis nihil. Aliquam, ipsa debitis?</textarea>
+                <div class="status_bar">✓ Saved</div>
+            `,
+            script: (stage, proc, file) => {
+                let target = getFile(file);
+
+                let textarea = document.querySelector(`#app_container .window[data-process="${proc.id}"] .content textarea`);
+                let status = document.querySelector(`#app_container .window[data-process="${proc.id}"] .content .status_bar`);
+                textarea.value = target.data;
+
+                proc.element.addEventListener('keydown', event => {
+                    if(event.key == 's' && event.ctrlKey) {
+                        event.preventDefault();
+                        saveTXT();
+                    }
+                });
+
+                textarea.addEventListener('input', () => {
+                    status.innerText = 'Unsaved changes';
+                });
+
+                function saveTXT() {
+                    // New file
+                    if(target.type == 'temp') {
+                        console.log('Create new file?');
+                    }
+                    target.data = textarea.value;
+                    status.innerText = '✓ Saved';
+                }
+            },
+
+            window: {
+                width: '400',
+                height: '600',
+            },
+        },
+        'images': {
+            title: 'Image Viewer',
+            icon: './assets/icon/Antu_google-draws.svg',
+            file_icon: './assets/icon/Antu_google-draws.svg',
+            html: /*html*/ `
+                <style>
+                    .app_images .content {
+                        padding: 6px 8px;
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                    }
+                    .app_images .content .viewer_image {
+
+                    }
+                </style>
+
+                <img src="" alt="" class="viewer_image">
+            `,
+            script: (stage, proc, file) => {
+                let target = getFile(file);
+
+                let image = document.querySelector(`#app_container .window[data-process="${proc.id}"] .content .viewer_image`);
+                image.src = target.data;
+            },
+
+            window: {
+                width: '800',
+                height: '600',
+            },
+        },
+        'dialog': {
+            title: 'Dialog',
+            icon: false,
+            html: /*html*/ `
+                <style>
+                    .app_dialog .content {
+                        padding: 6px 8px;
+                        text-align: center;
+                    }
+                    .app_dialog .content p {
+                        padding: 16px;
+                    }
+                    .app_dialog .content .dialog_buttons {
+                        display: flex;
+                    }
+                    .app_dialog .content button {
+                        color: black;
+                        width: 90%;
+                        background-color: #cccccc;
+                    }
+                </style>
+
+                <p>This is a dialog</p>
+                <div class="dialog_buttons">
+                    <button>Cancel</button>
+                    <button>OK</button>
+                </div>
+            `,
+            script: (proc, options) => {
+                let title = document.querySelector(`#app_container .window[data-process="${proc.id}"] .title_bar h2`);
+                let text = document.querySelector(`#app_container .window[data-process="${proc.id}"] .content p`);
+                title.innerText = options.title || 'Dialog';
+                text.innerText = options.text || '';
+            },
+
+            window: {
+                width: '400',
+                height: '140',
+
+                allow_actions: false,
+            }
+        }
+    },
+    'system': {
+        type: 'dir',
+
+        'config': {
+            file_association: {
+                'txt': 'notes',
+                'img': 'images'
+            },
+            dock: {
+                'pinned': [
+                    'files',
+                    'notes',
+                    'images',
+                ]
+            },
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,277 @@
+// Shorthand
+function style(element, classname, state) { state ? element.classList.add(classname) : element.classList.remove(classname); }
+
+
+var pX = 0;
+var pY = 0;
+
+// DOM
+const container = document.getElementById('app_container');
+
+// Variables
+const process_template = {
+    memory: {}, // data
+
+    max: false,
+    min: false,
+
+    width: '400',
+    height: '600',
+    x: 48,
+    y: 48,
+    z: 1,
+    allow_actions: true,
+
+    app: 'notes',
+    id: 1,
+    element: undefined,
+}
+var processes = {}
+var pid = 1;
+
+var dragging = false;
+let dragOffset = {
+    x: 0,
+    y: 0,
+}
+
+/** Takes directory string an returns file */
+function getFile(file) {
+    let target = file_system;
+
+    if(typeof file == 'string') {
+        let dir = file.split('/');
+        dir.shift();
+        for(i in dir) {
+            let f = dir[i];
+            // console.log(f);
+            target = target[f];
+        }
+    } else target = { data: '', type: 'temp', }
+    return target;
+}
+
+/** Launches an application */
+function launch(name='notes', file, options) {
+    let app = file_system.apps[name];
+
+    if(app == undefined) return console.warn('Application does not exist');
+
+    // Create process
+    let proc = processes[pid] = JSON.parse(JSON.stringify(process_template));
+    proc.app = name;
+    proc.id = pid;
+    proc.width = app.window.width;
+    proc.height = app.window.height;
+    proc.allow_actions = app.window.allow_actions;
+    // let offset = (Object.keys(processes).length || 1) * 24;
+    // console.log(offset);
+    // proc.x = offset;
+    // proc.y = offset;
+
+    // HTML
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = appHTML(app, proc);
+    container.append(wrapper.firstElementChild);
+    proc.element = document.querySelector(`#app_container .window[data-process="${proc.id}"]`);
+
+    // Title bar
+    let titlebar = document.querySelector(`#app_container .window[data-process="${proc.id}"] > .title_bar`);
+    titlebar.addEventListener('mousedown', event => {
+        dragging = proc.id;
+        // console.log(event.target.tagName);
+        if(event.target.tagName == 'BUTTON') return;
+        dragWindow(event, app, proc);
+    });
+    titlebar.addEventListener('dblclick', event => {
+        if(event.target.tagName == 'BUTTON') return;
+        windowAction('max', proc.id);
+    })
+
+    // Update dock
+    let dockicon = document.getElementById(`dock_${name}`);
+    if(dockicon != null) dockicon.classList.add('active');
+
+    // Script
+    if(app.script != undefined) app.script('start', proc, file);
+
+    pid++; // Increment
+    /** Template */
+    function appHTML(app, proc) {
+        let icon = app.icon == false ? '' : `<img src="${app.icon}" alt="" class="title_icon">`;
+        let title_buttons = proc.allow_actions == false ? '' : `
+        <button onclick="windowAction('min', ${proc.id})">_</button>
+        <button onclick="windowAction('max', ${proc.id})">[]</button>
+        <button class="button_close" onclick="windowAction('close', ${proc.id})">X</button>`;
+        return `
+        <div class="window app_${proc.app}" data-process="${proc.id}" data-app="${proc.app}" data-open-file="~/desktop/Example.txt" style="width: ${proc.width}px; height: ${proc.height}px; top: ${proc.y}; left: ${proc.x}; z-index: ${proc.z};">
+            <!-- Title bar -->
+            <div class="title_bar">
+                ${icon}
+                <h2>${app.title}</h2>
+                ${title_buttons}
+            </div>
+
+            <div class="content">
+                ${app.html}
+            </div>
+        </div>`;
+    }
+}
+
+/** Close/max/min window */
+function windowAction(action, id) {
+    if(action == undefined || id == undefined) return console.warn('Invalid window action');
+    let proc = processes[id];
+    let app = file_system.apps[proc.app];
+    if(proc == undefined) return console.warn(`Process (${id}) does not exist`);
+    // if(proc.allow_actions == false) return console.warn('This window has disabled this feature');
+
+    // Action
+    if(action == 'close') {
+        proc.element.classList.add('window_close');
+        proc.close_timer = setTimeout(() => {
+            proc.element.remove();
+            delete processes[proc.id];
+        }, 100);
+        let dockicon = document.getElementById(`dock_${proc.app}`);
+        if(dockicon != null) dockicon.classList.remove('active');
+    }
+    else if(action == 'max') {
+        proc.max = !proc.max;
+        style(proc.element, 'state_max', proc.max);
+    }
+    else if(action == 'min') {
+        proc.min = !proc.min;
+        style(proc.element, 'state_min', proc.min);
+    }
+    else console.warn('Invalid window action');
+}
+
+/** Drag window */
+function dragWindow(event, app, proc) {
+    if(dragging == false) return;
+    if(proc.max) windowAction('max', proc.id);
+
+    if(event != false) {
+        let rect = event.target.getBoundingClientRect();
+        dragOffset.x = event.clientX - rect.left;
+        dragOffset.y = event.clientY - rect.top;
+    }
+
+    let [x, y] = [pX - dragOffset.x, pY - dragOffset.y];
+
+    proc.x = x;
+    proc.y = y;
+    proc.element.style.left = `${x}px`;
+    proc.element.style.top = `${y}px`;
+
+    requestAnimationFrame(() => { dragWindow(false, app, proc); });
+}
+
+/** Toggles a system menu or overlay */
+function menu(id) {
+
+}
+
+/** Opens a file */
+function openFile(event) {
+    let element = event.srcElement;
+    if(element.tagName != 'FIGURE') element = element.parentNode;
+
+    let path = element.dataset.directory;
+
+    // Normal file
+    let file_ext = path.split('.')[1];
+    if(typeof file_ext != 'string') return console.warn('Invalid file extension');
+    // if(file_ext == undefined) {
+    //     let split = path.split('/');
+    //     let name = split[split.length];
+    //     return launch(name);
+    // }
+    let app = fileAssociation(path);
+    
+    launch(app, path);
+}
+
+/** Takes a path or filename and returns the associated app */
+function fileAssociation(path) {
+    let file_ext = path.split('.')[1];
+    return file_system.system.config.file_association[file_ext];
+}
+
+
+/** Populate dock */
+function populateDock() {
+    let pinned = file_system.system.config.dock.pinned;
+    let html = '';
+    for(i in pinned) {
+        let name = pinned[i];
+        let app = file_system.apps[name];
+        if(app == undefined) { console.warn('Dock: app doesnt exist'); continue; }
+        html += `
+        <button id="dock_${name}" class="dock_app" onclick="launch('${name}', undefined, { open_existing: true, })">
+            <img src=".${app.icon}" alt="">
+            <div class="dock_indicator"></div>
+        </button>`;
+    }
+
+    document.getElementById('dock_apps').innerHTML = html;
+}
+
+/** Populate folder */
+function populateFolder(path='~/desktop') {
+    let folder = getFile(path);
+    if(folder.type != 'dir') return console.warn(`(${path}) is not a folder`);
+    let html = '';
+
+    for(filename in folder) {
+        let file = folder[filename];
+        if(filename == 'type' && typeof file == 'string') continue;
+        html += template(path, filename, file);
+    }
+
+    // Export HTML
+    let containers = document.querySelectorAll(`.view_folder[data-directory="${path}"`);
+    containers.forEach(element => element.innerHTML = html);
+
+    /** Template */
+    function template(path, filename, file) {
+        let app = fileAssociation(filename);
+        let icon;
+        try { icon = file_system.apps[app]['file_icon']
+        } catch (error) {
+            console.warn(error);
+        }
+        if(icon == undefined) icon = './assets/icon/blank_document.png';
+        return `
+        <figure class="file" data-directory="${path}/${filename}" ondblclick="openFile(event)" role="button" tabindex="0">
+            <img src="${icon}" alt="${filename}">
+            <figcaption>${filename}</figcaption>
+        </figure>`;
+    }
+}
+
+
+
+
+document.addEventListener('mouseup', () => {
+    let proc = processes[dragging];
+    if(proc != null) {
+        if(proc.y < 0) proc.y = 0;
+        proc.element.style.left = `${proc.x}px`;
+        proc.element.style.top = `${proc.y}px`;
+    }
+    dragging = false;
+})
+
+// On load
+onload = () => {
+    populateFolder(path='~/desktop');
+    populateDock();
+}
+
+(onmousemove = event => {
+    pX = event.x;
+    pY = event.y;
+})()
